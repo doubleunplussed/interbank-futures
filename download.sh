7b93375e92c4a6c7defb3b9e133db285b20fd043
doubleunplussed <d.unplussed@gmail.com>
2023-02-03 00:00:06 +1100

    update

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@ set -euxo
 curl https://www.asx.com.au/data/trt/ib_expectation_curve_graph.pdf | python -c 'import sys,hashlib;open(f"pdfs/{hashlib.sha256(d:=sys.stdin.buffer.read()).hexdigest()}.pdf","wb").write(d)'
 python extract.py
 python process.py
-git add pdfs/* pdfdata.json processed_data.json
+git add pdfs pdfdata.json processed_data.json
 git commit -m update
 git push
 
