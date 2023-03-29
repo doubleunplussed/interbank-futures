447dc075a7a515dff16bfeed4d4dcd0f5e3dabe7
doubleunplussed <d.unplussed@gmail.com>
2023-03-29 22:05:01 +1100

    modify update script

--- update.sh
+++ update.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/bash
-set -euxo
+set -euo
+
 ./download.sh
-python extract.py
-python process.py
-git add pdfs pdfdata.json processed_data.json
-git commit -m update
-git push
+
+# Check if there are any untracked files in the "pdfs" directory
+untracked_files=$(git status --porcelain --untracked-files -- pdfs | grep '??')
+
+if [ -n "$untracked_files" ]; then
+  python extract.py
+  python process.py
+  git add pdfs pdfdata.json processed_data.json
+  git commit -m "update"
+  git push
+else
+  echo "No new file downloaded. Exiting."
+  exit 0
+fi
+
