f49dc490fdfb5a0f476c5f03c334ad4fd6be760b
doubleunplussed <d.unplussed@gmail.com>
2023-07-10 18:28:45 +1000

    re-enable auto-push

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ if [ -n "$new_data" ]; then
   python process.py
   git add raw_data.json processed_data.json
   git commit -m "update"
-  # git push
+  git push
 else
   echo "No new data. Exiting."
   exit 0
